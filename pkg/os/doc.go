a1ee7168644b6caf9392c07f1ebd35ae9e1c1f86
Stefan Genov <sgenov94@gmail.com>
2025-03-22 11:48:47 +0200

    feat(os): New os package
    - Handles os config

---
+++ pkg/os/doc.go
@@ -0,0 +1,16 @@
+/*
+os package contains information about the Current OS.
+
+It returns a single object that should not be modified externally, but should instead be used to fetch information.
+
+# How to use?
+
+You can do:
+
+```go
+software.GetOS()
+```
+
+after which point you will have an object with all the relevant data
+*/
+package os
---
+++ pkg/os/os.go
@@ -0,0 +1,32 @@
+package os
+
+import "runtime"
+
+// OS contains what the current OS is as well as different configuration that may be usefull
+// WARN: You should not use this directly, but instead use `software.GetOS()`. This struct is provided for mocking or to be used as an
+// argument
+type OS struct {
+	Distro      string
+	initialized bool
+}
+
+// os has a single instance.
+var os *OS = &OS{
+	Distro:      runtime.GOOS, // `linux`, `windows` or `darwin` ref: https://pkg.go.dev/runtime#pkg-constants
+	initialized: false,
+}
+
+// GetOs will return information about the OS, like distro, arch and configuration if needed.
+// This will
+func GetOS() *OS {
+	if !os.initialized {
+		initializeOS(os)
+	}
+
+	return os
+}
+
+func initializeOS(os *OS) {
+	// Do more config fetching here if needed
+	os.initialized = true
+}
