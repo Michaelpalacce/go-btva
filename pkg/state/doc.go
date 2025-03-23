b89d329e7b93bc77f14358ff17ff1f5843fd9013
Stefan Genov <sgenov94@gmail.com>
2025-03-23 03:36:11 +0200

    docs: Added a todo

--- pkg/state/doc.go
+++ pkg/state/doc.go
@@ -1,6 +1,6 @@
 /*
 The `state` package is used to contain the execution state of the program. It will save whatever is given to it and may or may not redirect it.
 
-TBD
+@TODO: Finish documetnation
 */
 package state
